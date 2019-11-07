45d40733d69bfcaa7a6d856adc7d5801590ced01
Cesar Gomez <zootechdrum@gmail.com>
2019-11-06 23:49:14 -0800

    added DOM logic to game also set up fierbase config

---
+++ app.js
@@ -0,0 +1,57 @@
+//Firebase configuration
+
+let player1 = '';
+let player2 = '';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBl9OIF6Y8mv58wJwccmY9hLOEQqqjXkT0",
+  authDomain: "testing-44b69.firebaseapp.com",
+  databaseURL: "https://testing-44b69.firebaseio.com",
+  projectId: "testing-44b69",
+  storageBucket: "testing-44b69.appspot.com",
+  messagingSenderId: "1000296696936",
+  appId: "1:1000296696936:web:8f5eb35049c30f06bf3fdd",
+  measurementId: "G-9J9SLR7K20"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+  var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is a special location provided by Firebase that is updated every time
+// the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+
+  // If they are connected..
+  if (snap.val()) {
+
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snapshot) {
+console.log(snapshot.numChildren())
+});
+
+$("#save-user").on('click', function(){
+  value = $('input').val().trim();
+
+  if(player1 === ''){
+    player1 = value;
+    console.log(player1);
+  }else{
+    player2 = value;
+  }
+  $('#myModal').modal('hide')
+});
